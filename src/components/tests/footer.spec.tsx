043153ad3e5acb33d2dec9e35ff083fde671c4db
renanleitev <renanleitev@gmail.com>
2023-03-29 00:22:36 -0300

    add: test for header

--- src/components/tests/footer.spec.tsx
+++ src/components/tests/footer.spec.tsx
@@ -3,9 +3,22 @@ import {screen, render} from '@testing-library/react';
 import Footer from "../Footer";
 
 describe('Testing Footer', () => {
-    it('should render footer', () => {
+    render(<Footer/>);
+    const getLinks = screen.getAllByRole('link', { hidden: true });
+    it('should be initial render', () => {
         const test = 't';
-        render(<Footer/>);
         expect(screen.queryByText(test)).toBeNull();
+    });
+    it('should render Github link', () => {
+        const githubLink = "https://github.com/renanleitev";
+        expect(getLinks[0].toString()).toEqual(githubLink); 
+    });
+    it('should render React link', () => {
+        const reactLink = "https://pt-br.reactjs.org/";
+        expect(getLinks[1].toString()).toEqual(reactLink); 
+    });
+    it('should render JSON Server link', () => {
+        const jsonLink = "https://github.com/typicode/json-server";
+        expect(getLinks[2].toString()).toEqual(jsonLink); 
     });
 });
