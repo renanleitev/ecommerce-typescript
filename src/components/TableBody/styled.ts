9ea0b6a900eba0e4aa21fcf0f771eac441438c38
Renan Vieira <renanleitev@gmail.com>
2023-05-02 14:21:42 -0300

    fix: form style on TableBody

--- src/components/TableBody/styled.ts
+++ src/components/TableBody/styled.ts
@@ -1,5 +1,42 @@
 import styled from "styled-components";
+import * as colors from '../../config/colors';
 
 export const HiddenTd = styled.td`
     display: ${props => (props.hidden ? `none`: `flex`)};
+    form {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        h1{
+            text-align: center;
+        }
+        .link{
+            margin-top: 10px;
+            font-size: 12px;
+        }
+        .link:hover{
+            color: ${colors.infoColor};
+        }
+        label {
+            margin: 10px 0px;
+            font-size: larger;
+            font-weight: bolder;
+            text-align: center;
+        }
+        input {
+            width: 100%;
+            padding: 20px;
+            border-radius: 20px;
+            border: 3px solid ${colors.primaryDarkColor};
+        }
+        button {
+            width: 20%;
+            margin-top: 10px;
+            align-self: center;
+            border-radius: 20px;
+        }
+        button:hover{
+            background-color: ${colors.infoColor};
+        }
+    }
 `;
