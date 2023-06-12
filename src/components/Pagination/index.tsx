28cc1a3fc22b112aeba1b04336bb653fa6059384
Renan Vieira <renanleitev@gmail.com>
2023-06-12 12:59:03 -0300

    add: pageNumber input and select

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -1,15 +1,17 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
 import * as interfaces from '../../interfaces';
-import { PaginationContainer } from './styled';
+import { PaginationContainer, NumberContainer, OptionContainer } from './styled';
 import { useDispatch } from 'react-redux';
 import { AppThunkDispatch } from '../../store';
 import { searchProductByName, showProductsPerPage } from '../../store/modules/products/reducer';
 import { showUsersPerPage } from '../../store/modules/login/reducer';
+import {FaBackward, FaForward} from 'react-icons/fa';
 
 const Pagination: React.FC<interfaces.Pagination> = (props: interfaces.Pagination) => {
     const dispatch = useDispatch<AppThunkDispatch>();
     const pageStatus = props.pageStatus;
     const pageNumbers: Array<number> = [];
+    const [numberOfPage, setNumberOfPage] = useState(0);
     function isProductDataType(object: interfaces.ProductData | interfaces.UserData): object is interfaces.ProductData {
         return true;
     }
@@ -29,17 +31,58 @@ const Pagination: React.FC<interfaces.Pagination> = (props: interfaces.Paginatio
         }
         props.setPageStatus({ ...pageStatus });
     }, [pageStatus]);
+    const handleChange = useCallback((event: React.FormEvent<HTMLSelectElement>) => {
+        setNumberOfPage(Number.parseInt(event.currentTarget.value));
+    }, []);
     return (
         <PaginationContainer>
-            {pageNumbers.map((numberOfPage: number) => {
-                return (
-                    <button
-                        key={numberOfPage + 1}
-                        onClick={() => paginationData(numberOfPage)}>
-                        {numberOfPage+1}
-                    </button>
-                )
-            })}
+        <NumberContainer>
+            <button
+                onClick={() => {
+                    if(numberOfPage !== 0){
+                        setNumberOfPage(numberOfPage-1);
+                    }
+                }}>
+                    <FaBackward/>
+            </button>
+            <button
+                key={numberOfPage+1}
+                onClick={() => paginationData(numberOfPage)}>
+                {numberOfPage+1}
+            </button>
+            <button
+                key={numberOfPage+2}
+                onClick={() => paginationData(numberOfPage+1)}>
+                {numberOfPage+2}
+            </button>
+            <button
+                onClick={() => {
+                    if((numberOfPage+2) <= pageNumbers.at(-1)){
+                        setNumberOfPage(numberOfPage+1);
+                    }
+                }}>
+                <FaForward/>
+            </button>
+        </NumberContainer>
+        <OptionContainer>
+            <p>Page Number</p>
+            <input
+            onChange={(event) => {
+                if (event.currentTarget.value !== ''){
+                    setNumberOfPage(Number.parseInt(event.currentTarget.value)-1);
+                }
+            }}
+            type="number" 
+            min={1} 
+            max={pageNumbers.length} 
+            defaultValue={1}
+            placeholder={'Page...'}/>
+            <select onChange={handleChange} size={2}>
+                {pageNumbers.map(page => 
+                    <option value={page}>{page+1}</option>
+                    )}
+            </select>
+        </OptionContainer>
         </PaginationContainer>
     )
 }
--- src/components/Pagination/styled.ts
+++ src/components/Pagination/styled.ts
@@ -3,9 +3,13 @@ import * as colors from '../../config/colors';
 
 export const PaginationContainer = styled.div`
     display: flex;
-    flex-direction: row;
+    flex-direction: column;
     align-items: center;
     justify-content: center;
+`;
+
+export const NumberContainer = styled(PaginationContainer)`
+    flex-direction: row;
     button{
         margin-top: 50px;
         margin-bottom: 80px;
@@ -14,5 +18,39 @@ export const PaginationContainer = styled.div`
     }
     button:hover{
         background-color: ${colors.infoColor};
+    }
+`;
+
+export const OptionContainer = styled(PaginationContainer)`
+    flex-direction: row;
+    height: 50px;
+    p{
+        height: inherit;
+        color: ${colors.primaryWhiteColor};
+        background-color: ${colors.primaryColor};
+        padding: 20px;
+        margin-right: 10px;
+        border-radius: 4px;
+    }
+    select, input{
+        height: inherit;
+        margin-top: 0;
+        border: none;
+        text-align: center;
+        width: 150px;
+        border-radius: 4px;
+        padding: 10px;
+        margin-right: 10px;
+        cursor: pointer;
+        ::-webkit-scrollbar {
+            width: 10px;
+            color: ${colors.primaryDarkColor};
+        }
+        ::-webkit-scrollbar-track {
+            box-shadow: inset 0 0 5px grey;
+        }
+        ::-webkit-scrollbar-thumb {
+            background: ${colors.primaryColor};
+        }
     }
 `;
